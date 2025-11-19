699336b448683920527f40937af28208bc3f118c
nozila2024 <nozilarajabboyeva@gmail.com>
2025-11-19 15:37:22 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+#1-misol
+n = "GrokByxAI"
+print(n[:4],n[6:])
+
+#2-misol
+n = "9876543210"
+t = n[::2]
+print(t[::-1])
+
+#3-misol
+n = "MachineLearning"
+print(n[8:13])
+
+#4-misol
+n = "PythonJavaJavaScript"
+print(n[6:14])
+
+#5-misol
+n = "abcdefghijk"
+toza = n[:5] + n[-5:]
+print(toza)
+
+#6-misol
+s = "OpenAIxAIGooleAI"
+print(s)
+
+soni = s.count("AI")
+print(f"AI lar soni: {soni}")
+
+res = "AI" * soni
+print(res)
+
+#7-misol
+n = "1234567890abc"
+print(n[:10], n[-3:])
